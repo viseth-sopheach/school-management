0da7f17e2659f88e94afa7217900f2cc4b4b3f03
Viseth <visethsopheach@gmail.com>
2026-07-20 12:05:28 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,36 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+   use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+   /**
+    * Seed the application's database.
+    */
+   public function run(): void
+   {
+      User::factory()->create([
+         'name' => 'Viseth',
+         'email' => 'viseth@example.com',
+         'password' => Hash::make('123'),
+         'role' => 'admin',
+      ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+      User::factory()->create([
+         'name' => 'John',
+         'email' => 'john@example.com',
+         'password' => Hash::make('123'),
+         'role' => 'teacher',
+      ]);
+
+      User::factory()->create([
+         'name' => 'Tom',
+         'email' => 'tom@example.com',
+         'password' => Hash::make('123'),
+         'role' => 'student',
+      ]);
+   }
 }
