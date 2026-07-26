ec61994d23a326de858c000de2582061bb8f12c4
Viseth <visethsopheach@gmail.com>
2026-07-26 15:30:10 +0700

    fix API

--- config/cors.php
+++ config/cors.php
@@ -5,6 +5,10 @@ $frontendUrls = array_values(array_unique(array_filter(array_map(
    explode(',', env('FRONTEND_URL', ''))
 ))));
 
+$frontendUrls[] = 'http://localhost:5173';
+$frontendUrls[] = 'http://localhost:3000';
+$frontendUrls[] = 'http://localhost:8000';
+
 return [
    'paths' => ['api/*', 'sanctum/csrf-cookie'],
    'allowed_methods' => ['*'],
